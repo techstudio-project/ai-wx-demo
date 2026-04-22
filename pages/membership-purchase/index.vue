5ea0d8c6d3bc1c00b90a474c45da48c987583b5f
techstudio-project <254483591@qq.com>
2026-04-22 14:42:59 +0800

    style: 调整会员卡片层级与服务列表间距

---
+++ pages/membership-purchase/index.vue
@@ -0,0 +1,32 @@
+<template>
+	<view class="simple-page">
+		<view class="title">开通会员</view>
+		<view class="desc">此页面用于展示会员套餐、权益与购买流程。</view>
+	</view>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.simple-page {
+	min-height: 100vh;
+	padding: 32rpx;
+	background: #f7f8fc;
+	box-sizing: border-box;
+}
+
+.title {
+	font-size: 36rpx;
+	font-weight: 600;
+	color: #1f2333;
+}
+
+.desc {
+	margin-top: 18rpx;
+	font-size: 26rpx;
+	color: #6e7486;
+	line-height: 1.7;
+}
+</style>
